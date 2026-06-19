c3bc999e6511c6c0eff0f9694641607ca4c10435
Lazaro <13026853+LazaroOnline@users.noreply.github.com>
2026-06-19 08:50:48 +0200

    Add VSCode web version support in /dist/web

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import * as beautify from 'js-beautify'
+import beautify = require('js-beautify');
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
 
